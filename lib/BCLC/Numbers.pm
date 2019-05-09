bf849dc50a207396bea9b9316d3cf99cf5ac60aa
Steve Bertrand <steveb@cpan.org>
2019-05-09 09:47:57 -0700

    added initial Perl distribution files

---
+++ lib/BCLC/Numbers.pm
@@ -0,0 +1,19 @@
+package BCLC::Numbers;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+BCLC::Numbers - Calculate 649 historical wins
+
+=head1 AUTHOR
+
+Steve Bertrand, C<< <steve.bertrand at gmail.com> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2019 Steve Bertrand.
+
