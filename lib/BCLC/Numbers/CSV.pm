a5cef53a8671ffc6772e37b6576b60bfb31176ea
Steve Bertrand <steveb@cpan.org>
2019-05-13 15:17:30 -0700

    added initial doc files for CSV module

--- lib/BCLC/Numbers/CSV.pm
+++ lib/BCLC/Numbers/CSV.pm
@@ -50,3 +50,46 @@ sub retrieve {
 1;
 
 __END__
+
+=head1 NAME
+
+BCLC::Numbers::CSV - Provides CSV file backend support for L<BCLC::Numbers>
+L<Dancer2> web application
+
+=head1 DESCRIPTION
+
+Object Oriented module that provides CSV file access for L<BCLC::Numbers>
+
+=head1 METHODS
+
+=head2 new($csv_file)
+
+Instantiates and returns a new L<BCLC::Numbers::CSV> object, ready to process
+requests.
+
+Parameters:
+
+    $csv_file
+
+Mandatory, String: The path and file name of the CSV file.
+
+Return: An object of class L<BCLC::Numbers::CSV>
+
+=head2 retrieve
+
+Compiles the entire Lotto 649 historical draw information, using the CSV file.
+
+Takes no parameters.
+
+Return: An Array Reference of Hash References, where each hash ref contains
+the dataset's column name as the key, and its associated value as the value.
+
+=head1 AUTHOR
+
+Steve Bertrand, C<< <steve.bertrand at gmail.com> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2019 Steve Bertrand.
+
+Released under the Apache License, Version 2
